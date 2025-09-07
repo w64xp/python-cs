794c6d775fb08dab646e89a2eba1760c70522d72
w64xp <674245007@gmail.com>
2025-09-07 23:31:00 +0700

    edit student.py add file path

--- week9/txt_file/student_01.py
+++ week9/txt_file/student_01.py
@@ -1,37 +1,44 @@
 import os
+
 class TXT:
+    filepath = r"C:\Users\Byt3sp1\student.txt"
+    
     def Create():
         text_data = """New file create"""
-        with open("student.txt", "w", encoding="utf-8") as file:
+        with open(TXT.filepath, "w", encoding="utf-8") as file:
             try:
                 file.write(text_data)
-                print("New file create")
+                print("New file created")
             except:
                 print("can't Save")
+
     def Reader():
-        with open("student.txt", "r", encoding="utf-8") as file:
+        with open(TXT.filepath, "r", encoding="utf-8") as file:
             try:
                 print(file.read())
             except:
                 print("can't read")
+
     def Updated(data_update):
-        with open("student.txt", "w", encoding="utf-8") as file:
+        with open(TXT.filepath, "w", encoding="utf-8") as file:
             try:
                 file.write(data_update)
                 print("update")
             except:
                 print("can't update")
-    def Del(fileName):
-        file = fileName
-        if(os.path.exists(file)):
-            os.remove(file)
+
+    def Del():
+        if os.path.exists(TXT.filepath):
+            os.remove(TXT.filepath)
+            print("Deleted")
         else:
-            print(f"Not found : {file}")
+            print(f"Not found : {TXT.filepath}")
+
 
 #----------------------------
 while True:
     print("<--------------------------------------------------Menu-------------------------------------------------->")
-    print("Q = Quit,C = Create,R = Read, Fetch, Quary, Select (index),U = Update,D = Delete (index)")
+    print("Q = Quit, C = Create, R = Read, U = Update, D = Delete")
     print("<--------------------------------------------------Menu-------------------------------------------------->")
     status = input("Select : ")
     if(status.lower()=="q"):
@@ -41,7 +48,7 @@ while True:
     elif(status.lower()=="r"):
         TXT.Reader()
     elif(status.lower()=="u"):
-        inp = input("Data_input:")
+        inp = input("Data_input: ")
         TXT.Updated(inp)
     elif(status.lower()=="d"):
-        TXT.Del("student.txt")
+        TXT.Del()
